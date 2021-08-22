26b95627f77547b3dd11a00d6b449734e7646eb7
programmerSaykat <all.developer20@gmail.com>
2021-08-23 04:47:58 +0600

    stepToInputValue

--- js/script.js
+++ js/script.js
@@ -18,19 +18,40 @@ document.getElementById('generate-pin').addEventListener('click', function(){
 
 });
 
-
-// document.getElementById('generate-pin').addEventListener('click', function(){
-//     const pin = Math.round(Math.random()*100000)
-//     const pinString = pin + '';
-//     if (pinString.length == 5) {
-//         return pin;
-//     }
-//     else{
-//         console.log('thsi si 4 down', pin);
-//         return pin;
+/* Use My taktis 
+document.getElementById('generate-pin').addEventListener('click', function(){
+    const pin = Math.round(Math.random()*100000)
+    const pinString = pin + '';
+    if (pinString.length == 5) {
+        return pin;
+    }
+    else{
+        console.log('thsi si 4 down', pin);
+        return pin;
         
-//     }
-// });
+    }
+}); */
+
+
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('type-numbers');
+    if(isNaN(number)){
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+        
+    }
+    else{
+        const privouseNumber = calcInput.value;
+        const newCalc = privouseNumber + number;
+        calcInput.value = newCalc;
+    }
+
+
+    
+})
+
 
 
 
